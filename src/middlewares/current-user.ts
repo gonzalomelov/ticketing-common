8400715eeb6d2cc6854afd8bfaf628f2615ffe7a
Gonzalo Melo <gonzalomelov@gmail.com>
2024-05-24 15:22:59 -0300

    Remove unused import

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 import { CustomError } from '../errors/custom-error';
 
 export interface UserPayload {
